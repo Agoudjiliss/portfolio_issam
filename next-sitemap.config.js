f98e7c210010483353dcaced3f3e52264c35db13
issam agoudjil <95285029+Agoudjiliss@users.noreply.github.com>
2024-01-16 13:41:59 +0100

    Update next-sitemap.config.js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: process.env.SITE_URL || 'https://example.com',
+    siteUrl: process.env.SITE_URL || 'https://issam-agoudjil.site',
     generateRobotsTxt: true, // (optional)
     outDir: "./out",
 }
