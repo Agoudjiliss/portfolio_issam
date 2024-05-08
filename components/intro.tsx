d972b0996b5b710b861a4b068de300312251ab75
issam agoudjil <95285029+Agoudjiliss@users.noreply.github.com>
2024-05-08 20:16:06 +0100

    Update intro.tsx

--- components/intro.tsx
+++ components/intro.tsx
@@ -92,10 +92,10 @@ export default function Intro() {
 
         <a
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
-          href="/CV.pdf"
+          href="/issam-agoudjil_CV.pdf"
           download
         >
-          Download CV{"./app/issam-agoudjil_CV.pdf"}
+          Download CV{""}
           <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
         </a>
 
